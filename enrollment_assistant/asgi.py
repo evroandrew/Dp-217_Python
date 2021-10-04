b8d39b8eb9f6a126ca182e061d0fbd9c5ed74fd8
Pahan <5972140@gmail.com>
2021-10-04 15:45:09 +0300

    Project rename

--- enrollment_assistant/asgi.py
+++ enrollment_assistant/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Enrollment Assistant project.
+ASGI config for enrollment_assistant project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Enrollment Assistant.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enrollment_assistant.settings')
 
 application = get_asgi_application()
