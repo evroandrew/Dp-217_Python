fca315dd7260ca4e1290e6c9194e46677e3ac808
evroandrew <volotkov@i.ua>
2021-10-07 14:19:08 +0300

    done page with the conclusion of the questioning result
    done ajax
    
    done view
    
    rename
    
    rename
    
    refactored
    
    redone button styles

--- enrollment_assistant/urls.py
+++ enrollment_assistant/urls.py
@@ -20,4 +20,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index_view, name='index'),
+    path('questioning/', views.questioning_view, name='questioning'),
+    path('questioning_ajax/', views.questioning_ajax, name='questioning_ajax'),
+    path('questioning_results/', views.questioning_results, name='questioning_results'),
 ]
