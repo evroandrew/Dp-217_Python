b04aba7c9e2b3974dc264a0e1e263af55bca3a92
suslovichb <suslovichb@gmail.com>
2021-10-07 13:41:29 +0300

    Add custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    GENDER_CHOICES = (
+        ('male', 'male'),
+        ('female', 'female'),
+        ('undefined', 'undefined')
+    )
+
+    gender = models.CharField(choices=GENDER_CHOICES, default='undefined', max_length=9)
+    city = models.CharField(blank=True, default='', max_length=100)
--- users/views.py
+++ users/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def profile_view(request):
-    return render(request, 'users/profile.html')
+    return render(request, 'users/profile.html', {'user': request.user})
