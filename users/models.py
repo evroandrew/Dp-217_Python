0d77632f6117713bcc530f4d1f98cb7665f908e6
suslovichb <suslovichb@gmail.com>
2021-10-12 14:44:43 +0300

    Change CustomUser model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
@@ -11,3 +13,14 @@ class CustomUser(AbstractUser):
 
     gender = models.CharField(choices=GENDER_CHOICES, default='undefined', max_length=9)
     city = models.CharField(blank=True, default='', max_length=100)
+    phone = models.CharField(blank=True, default='', max_length=12)
+    email = models.EmailField(_('email_address'), unique=True)
+    # username = models.CharField(blank=True, default='', max_length=20)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
