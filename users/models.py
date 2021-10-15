6ed3069fe3dfbe6cab65cb302ed105e9f388a733
suslovichb <suslovichb@gmail.com>
2021-10-15 16:04:51 +0300

    Refactor users app

--- users/models.py
+++ users/models.py
@@ -13,4 +13,4 @@ class CustomUser(AbstractUser):
     gender = models.CharField(choices=GENDER_CHOICES, default='undefined', max_length=9)
     city = models.CharField(blank=True, default='', max_length=100)
     phone = models.CharField(blank=True, default='', max_length=12)
-    email = models.EmailField(_('email'), unique=True, )
+    email = models.EmailField(unique=True)
