6a9f2e73bd1a9ebca2cfed2783e47cfab3daff01
Artem Marhaza <artemmrgz@gmail.com>
2021-10-15 19:50:47 +0200

    add js to univer search form template

--- universearch/urls.py
+++ universearch/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.uni_search),
+    path('region-data/', views.get_json_regions_data),
+    path('cities-data/<str:region>/', views.get_json_cities_data),
+    path('fields-data/', views.get_json_fields_data),
+    path('specialities-data/<str:field>/', views.get_json_specs_data),
 ]
--- universearch/views.py
+++ universearch/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Region, City, StudyField, Speciality
+
+
+def get_json_regions_data(request):
+    region_values = list(Region.objects.values())
+    return JsonResponse({'data': region_values})
+
+
+def get_json_cities_data(request, *args, **kwargs):
+    selected_region = kwargs.get('region')
+    cities_values = list(City.objects.filter(region__name=selected_region).values())
+    return JsonResponse({'data': cities_values})
+
+
+def get_json_fields_data(request):
+    values = list(StudyField.objects.values())
+    return JsonResponse({'data': values})
+
+
+def get_json_specs_data(request, *args, **kwargs):
+    selected_field = kwargs.get('field')
+    specs_values = list(Speciality.objects.filter(study_field__name=selected_field).values())
+    return JsonResponse({'data': specs_values})
 
 
 def uni_search(request):
     return render(request, 'universearch/search_form.html')
+
