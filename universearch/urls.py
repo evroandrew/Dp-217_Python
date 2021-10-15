91ef75a33a5bf3262cdf4b2894019d9e73cba26d
Artem Marhaza <artemmrgz@gmail.com>
2021-10-15 19:34:22 +0200

    add univers search form

--- universearch/urls.py
+++ universearch/urls.py
@@ -4,7 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.uni_search),
-    path('region-data/', views.get_json_regions_data),
-    path('cities-data/<str:region>/', views.get_json_cities_data),
-    path('fields-data/', views.get_json_fields_data),
 ]
--- universearch/views.py
+++ universearch/views.py
@@ -1,51 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from .models import Region, City, StudyField
-from .pars import get_cities, get_regions, SITE_URL
-
-
-def populate_db_regions():
-    regions = get_regions(SITE_URL)
-    for region in regions:
-        reg = Region(name=region)
-        reg.save()
-
-
-def populate_db_cities():
-    cities_li = get_cities(SITE_URL)
-    for region, cities in cities_li.items():
-        region = Region.objects.filter(name=region).first()
-
-        for city in cities:
-            city_obj = City(name=city, region=region)
-            city_obj.save()
-
-
-def get_json_regions_data(request):
-    region_values = list(Region.objects.values())
-    return JsonResponse({'data': region_values})
-
-
-def get_json_cities_data(request, *args, **kwargs):
-    selected_region = kwargs.get('region')
-    if selected_region == 'Всi областi':
-        cities_values = list(City.objects.values())
-    else:
-        cities_values = list(City.objects.filter(region__name=selected_region).values())
-    return JsonResponse({'data': cities_values})
-
-
-def get_json_fields_data(request):
-    values = list(StudyField.objects.values())
-    return JsonResponse({'data': values})
 
 
 def uni_search(request):
-    # regs = Region.objects.all()
-    # cities = City.objects.all()
-    #
-    # if not regs:
-    #     populate_db_regions()
-    # if not cities:
-    #     populate_db_cities()
-    return render(request, 'uni_search.html')
+    return render(request, 'universearch/search_form.html')
