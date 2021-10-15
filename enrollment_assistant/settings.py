f6266c76d3ac3a20806ba022d660f38f6015fa56
Pahan <5972140@gmail.com>
2021-10-15 14:58:09 +0300

    adding secure db connection

--- enrollment_assistant/settings.py
+++ enrollment_assistant/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'enrollment_assistant.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', "localhost"),
-        'USER': os.environ.get('DB_USER', "localhost"),
-        'PASSWORD': os.environ.get('DB_PASS', "localhost"),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST', "localhost"),
         'PORT': os.environ.get("DB_PORT", '5432'),
     }
