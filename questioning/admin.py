9311bdafed005b27ff1d86657ea5193ffad87411
evroandrew <volotkov@i.ua>
2021-10-16 11:40:43 +0300

    registered questioning models

--- questioning/admin.py
+++ questioning/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TestResult, UserTestResult
 
-# Register your models here.
+admin.site.register(TestResult)
+admin.site.register(UserTestResult)
