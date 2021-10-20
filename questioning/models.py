f7e065c2feabf4e4ec473884952038b5745e16a8
evroandrew <volotkov@i.ua>
2021-10-20 21:37:01 +0300

    refactored for QuestionsBase

--- questioning/models.py
+++ questioning/models.py
@@ -35,15 +35,15 @@ class UserTestResult(models.Model):
 
     def __str__(self):
         return f"{self.result_id}"
-      
-      
+
+
 class QuestionsBase(models.Model):
-    type = 1
     question = models.TextField("Запитання", blank=True)
     answer_1 = models.TextField("Відповідь 1", blank=True)
     answer_2 = models.TextField("Відповідь 2", blank=True)
     result_id_1 = models.PositiveSmallIntegerField("Індекс відповіді 1", null=True)
     result_id_2 = models.PositiveSmallIntegerField("Індекс відповіді 2", null=True)
+    type = models.PositiveSmallIntegerField(default=1)
 
     class Meta:
         verbose_name = "Запитання"
