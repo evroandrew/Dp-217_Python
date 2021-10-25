908fc31d36cd1f150626a570cc3486b904920be4
suslovichb <suslovichb@gmail.com>
2021-10-25 11:22:19 +0300

    Update user model

--- universearch/admin.py
+++ universearch/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import City, Region, StudyField, Speciality, University
 
-# Register your models here.
+
+admin.site.register(City)
+admin.site.register(Region)
+admin.site.register(StudyField)
+admin.site.register(Speciality)
+admin.site.register(University)
