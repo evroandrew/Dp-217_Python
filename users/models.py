d7d7cddb4dd8cecc24b00fa260b49f52e081076b
suslovichb <suslovichb@gmail.com>
2021-10-27 20:45:32 +0300

    Improve user model and profile update form

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from universearch.models import City
 
 
@@ -10,8 +11,13 @@ class CustomUser(AbstractUser):
         ('н.в.', 'не визначено')
     )
 
+    phone_validator = RegexValidator(
+        regex=r'^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){12}(\s*)?$',
+        message="Введіть номер телефону в міжнародному форматі."
+    )
+
     gender = models.CharField(choices=GENDER_CHOICES, default='н.в.', max_length=9)
     city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, related_name='users', blank=True)
-    phone = models.CharField(blank=True, default='', max_length=12)
+    phone = models.CharField(blank=True, max_length=37, validators=[phone_validator])
     email = models.EmailField(unique=True)
     deletion_request_date = models.DateTimeField(null=True)
