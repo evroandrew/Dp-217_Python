2d2a6f805aef4446acbb84c7e84077eae5d11687
Artem Marhaza <artemmrgz@gmail.com>
2021-10-27 16:12:14 +0200

    add str method to StudyField model

--- universearch/models.py
+++ universearch/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class StudyField(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Speciality(models.Model):
     name = models.CharField(max_length=200)
