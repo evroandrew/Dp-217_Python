2e7d33aa40e02f7be263f9233eccf0d8b24fc35b
Андрей Волотковский <evroandrew@gmail.com>
2021-11-29 15:21:24 +0200

    Dp217 82 fix links to universearch while service is translated (#71)
    * fixed en locale fixtures
    
    * fixed en locale fixtures
    
    * fixed locale for api request
    
    Co-authored-by: evroandrew <volotkov@i.ua>

--- universearch/services.py
+++ universearch/services.py
@@ -2,19 +2,32 @@ import os
 import requests
 from requests.exceptions import ConnectionError
 from django.core.cache import cache
+from .models import Region, StudyField, City, Speciality
+
+DATABASES = {
+    'region': Region,
+    'city': City,
+    'field': StudyField,
+    'speciality': Speciality,
+}
+
+
+def params_translation(params):
+    translated_params = {DATABASES[key].objects.filter(name=value).first().name_uk for key, value in params.items() if
+                         DATABASES.get(key)}
+    return translated_params
 
 
 def get_universities_api(region: str, city: str, field: str, speciality: str) -> list:
     base_url = os.environ.get("UNI_PARSE_URL")
     url = os.path.join(base_url, 'univerdata/')
-    print(url)
     # url = "http://127.0.0.1:8080/
-    params = {
+    params = params_translation({
         'region': region,
         'city': city,
         'field': field,
         'speciality': speciality
-    }
+    })
     try:
         if cache.get(params):
             response = cache.get(params)
@@ -39,4 +52,3 @@ def get_universities(univers_ids):
         return response.json()
     except Exception:
         return
-
