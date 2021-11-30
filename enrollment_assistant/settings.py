62aa4435a248774e1e3581de7b0b6d5ce9ecb253
evroandrew <volotkov@i.ua>
2021-11-30 16:54:26 +0200

    added static pathes

--- enrollment_assistant/settings.py
+++ enrollment_assistant/settings.py
@@ -17,6 +17,9 @@ from pathlib import Path
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
