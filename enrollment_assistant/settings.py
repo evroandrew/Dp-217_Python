5567d0f64b8b21a178d7cc2df6a95547d70fc7c3
Artem Marhaza <artemmrgz@gmail.com>
2021-12-07 11:44:56 +0100

    add middleware

--- enrollment_assistant/settings.py
+++ enrollment_assistant/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'enrollment_assistant.middlewares.CustomMiddleware',
 ]
 
 ROOT_URLCONF = 'enrollment_assistant.urls'
